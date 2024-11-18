99ab48ec0a6c3ab7ae4d236393cdeb2f6fdf2829
Kyrylo Lapchenko <kyrylo.lap@gmail.com>
2024-11-18 19:27:41 +0100

    Adjust login command to return error when user exists

--- command.go
+++ command.go
@@ -18,7 +18,14 @@ func handlerLogin(s *state, cmd command) error {
     if len(cmd.args) == 0 {
         return fmt.Errorf("0 arguments, expecting 1 {username}")
     }
-    err := s.cfg.SetUser(cmd.args[0])
+    
+    name := cmd.args[0]
+    _, err := s.db.GetUser(context.Background(), name)
+    if err != nil {
+        return fmt.Errorf("Cannot login user which doesn't exist")
+    }
+
+    err = s.cfg.SetUser(name)
     if err != nil {
         return err
     }
